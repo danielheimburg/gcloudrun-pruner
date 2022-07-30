f874c1a61fd9e0cc649e9af373149f27f120a80b
Daniel Heimburg <daniel.heimburg@gmail.com>
2022-07-30 07:23:31 +0200

    refactor

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 
 _term() {
-  rm *-rev.txt
+  rm ${TMPDIR}/*-rev.txt
   exit 0
 }
 
@@ -14,23 +14,28 @@ then
   export $(cat .env | xargs)
 fi
 
+GCLOUDARGS="--project=${PROJECT} --verbosity=error --quiet"
+TMPDIR="/tmp"
+
 echo "running job.."
 
-services=$(gcloud --project=${PROJECT} run services list --format="value(metadata.name)")
+services=$(gcloud ${GCLOUDARGS} run services list --format="value(metadata.name)")
 
 for service in $services; do
 
-    gcloud --project=${PROJECT} run revisions list --service=${service} --region=europe-west9 --filter="metadata.creationTimestamp<-${DELETE_OLDER}" --sort-by=metadata.creationTimestamp --format="value(metadata.name, status.imageDigest)" > ${service}-rev.txt
+    gcloud ${GCLOUDARGS} run revisions list --service=${service} --region=europe-west9 --filter="metadata.creationTimestamp<-${DELETE_OLDER}" --sort-by=metadata.creationTimestamp --format="value(metadata.name, status.imageDigest)" > ${TMPDIR}/${service}-rev.txt
 
-    if [ $(cat ${service}-rev.txt|wc -l) -ge ${KEEP} ]; then
+    if [ $(cat ${TMPDIR}/${service}-rev.txt|wc -l) -ge ${KEEP} ]; then
         echo "cleaning ${service}"
         while read -r line; do
           read -r revision digest <<< $line
-          gcloud --project=${PROJECT} --quiet run revisions delete ${revision}
-          gcloud --project=${PROJECT} --quiet container images delete --force-delete-tags ${digest};
+          gcloud ${GCLOUDARGS} run revisions delete ${revision}
+          gcloud ${GCLOUDARGS} container images delete --force-delete-tags ${digest};
         done < ${service}-rev.txt
+    else
+        echo "no cleanup needed in ${service}"
     fi
-    rm ${service}-rev.txt
+    rm ${TMPDIR}/${service}-rev.txt
 done
 
 echo "job done"
